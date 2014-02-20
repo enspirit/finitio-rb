a867a7f989108cee12e4a8f71de57bac8a2b3cba
Bernard Lambeau <blambeau@gmail.com>
2014-02-20 18:14:28 +0100

    Document BuiltinType.

--- lib/qrb/type/builtin_type.rb
+++ lib/qrb/type/builtin_type.rb
@@ -1,4 +1,19 @@
 module Qrb
+  #
+  # A BuiltinType generator allows mapping an information type from Q to a
+  # type of the host language, here Ruby. For instance,
+  #
+  #     Int := BuiltinType(ruby.Fixnum)
+  #
+  # The set of values captured by the information type is the same set of
+  # values that can be represented by the host type. In the example, `Int`
+  # captures the same set of numbers as ruby's Fixnum.
+  #
+  # Once uped, the representation of the information type is done through
+  # the ruby class of the host type. In the example:
+  #
+  #    M(Int) = Fixnum
+  #
   class BuiltinType < Type
 
     def initialize(ruby_type, name = nil)
@@ -7,6 +22,8 @@ module Qrb
     end
     attr_reader :ruby_type
 
+    # Check that `value` is a valid instance of `ruby_type` through `===` or
+    # raise an error.
     def up(value, handler = UpHandler.new)
       handler.failed!(self, value) unless ruby_type===value
       value
