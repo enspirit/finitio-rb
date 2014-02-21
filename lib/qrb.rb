9d3b91af43aeb5d0d6e856d8577a279c6af971fb
Bernard Lambeau <blambeau@gmail.com>
2014-02-21 11:21:23 +0100

    Move non-first-class abstractions under support/

--- lib/qrb.rb
+++ lib/qrb.rb
@@ -9,8 +9,6 @@ end # module Qrb
 
 require_relative "qrb/version"
 require_relative "qrb/errors"
-require_relative 'qrb/attribute'
-require_relative 'qrb/heading'
+require_relative "qrb/support"
 require_relative 'qrb/type'
-require_relative 'qrb/up_handler'
 require_relative 'qrb/ruby_dsl'
---
+++ lib/qrb/support.rb
@@ -0,0 +1,3 @@
+require_relative 'support/attribute'
+require_relative 'support/heading'
+require_relative 'support/up_handler'
