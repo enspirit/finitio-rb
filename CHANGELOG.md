57a83e6a636bcc02cddb2f76948fd39647f151ba
Bernard Lambeau <blambeau@gmail.com>
2014-03-04 13:55:20 +0100

    Releasing v0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.2.0 / 2014-03-04
+
+* Fix dependencies in gemspec (judofyr)
+
 # 0.1.0 / 2014-03-03
 
 * Enhancements
--- lib/qrb/version.rb
+++ lib/qrb/version.rb
@@ -2,7 +2,7 @@ module Qrb
   module Version
 
     MAJOR = 0
-    MINOR = 1
+    MINOR = 2
     TINY  = 0
 
     def self.to_s
