15466aba7524f3b59b121b2e103e5e3378763e14
Bernard Lambeau <blambeau@gmail.com>
2019-03-23 09:36:15 +0100

    Add a base Type inference feature.

---
+++ lib/finitio/inference.rb
@@ -0,0 +1,51 @@
+module Finitio
+  class Inference
+
+    def initialize(system, options = {})
+      @system = system
+      @options = options
+    end
+    attr_reader :system
+
+    def call(input)
+      infer_type(input)
+    end
+
+  private
+
+    def infer_type(value)
+      case value
+      when Hash
+        attrs = value.map{|k,v|
+          Attribute.new(k, infer_type(v))
+        }
+        heading = Heading.new(attrs)
+        TupleType.new(heading)
+      when Array
+        infered = value.inject(nil){|sup, value|
+          value_type = infer_type(value)
+          sup.nil? ? value_type : sup.suppremum(value_type)
+        }
+        SeqType.new(infered)
+      else
+        found = self.system.types.values.find{|t|
+          try_dress(t, value)
+        }
+        found ? found : ANY_TYPE
+      end
+    end
+
+    def try_dress(t, value)
+      raise "Type expected, got #{t}" unless t.is_a?(Type)
+      t.dress(value)
+      true
+    rescue Finitio::Error => ex
+      #puts %Q{[#{ex.class}] #{ex.message}}
+      nil
+    rescue => ex
+      puts %Q{[#{ex.class}] #{ex.message}\n#{ex.backtrace.join("\n")}}
+      nil
+    end
+
+  end # class Inference
+end # module Finitio
